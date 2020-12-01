a8b15b1b6791fbe8bc117ab4ba7a427f3b20b8de
MarkusSeiberl <markus_seiberl@live.at>
2020-12-01 16:11:18 +0100

    use of fire instead of clickOn

--- src/test/java/at/fhhgb/team/a/elevators/app/AppTest.java
+++ src/test/java/at/fhhgb/team/a/elevators/app/AppTest.java
@@ -46,11 +46,12 @@ public class AppTest {
         verifyThat(robot.lookup("#modeButton").queryAs(Button.class), LabeledMatchers.hasText("Auto"));
 
         Button btn = robot.lookup("#modeButton").queryAs(Button.class);
-        robot.clickOn(btn);
-        WaitForAsyncUtils.waitForFxEvents();
-        verifyThat(btn, LabeledMatchers.hasText("Auto"));  //this should fail: should be "Manual"
-        robot.clickOn(btn);
-        WaitForAsyncUtils.waitForFxEvents();
+        btn.fire();
+//        robot.clickOn(btn);
+//        WaitForAsyncUtils.waitForFxEvents();
+//        verifyThat(btn, LabeledMatchers.hasText("Auto"));  //this should fail: should be "Manual"
+//        robot.clickOn(btn);
+//        WaitForAsyncUtils.waitForFxEvents();
 
         verifyThat(btn, isVisible());
         verifyThat(btn, LabeledMatchers.hasText("Manual"));  //this should fail: should be "Auto"
